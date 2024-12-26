7a8b677a78c8d58dc87c487fd92717f67b1d16f5
andriAntt <andriy.ant.work@gmail.com>
2024-12-26 17:58:24 +0200

    fixed config

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -3,7 +3,7 @@ module.exports = {
   apps: [
     {
       name: 'fxn-connector',
-      script: './src/index.js',
+      script: './dist/src/index.js',
       instances: '1',
       exec_mode: 'cluster',
       max_restarts: 10,
